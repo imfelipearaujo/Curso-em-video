add055dddab802a621d9975b98c68e8c84fc2d42
Felipe Araujo <felipearaujodeassis@gmail.com>
2022-12-25 14:08:35 -0300

    Update Ex091.py

--- Python/Ex091.py
+++ Python/Ex091.py
@@ -14,7 +14,8 @@ for k, v in jogo.items():
     print(f'{k} tirou {v} no dado.')
     sleep(0.5)
 ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
-print('Ranking de jogadores:')
+print('-=' * 30)
+print('== RANKING DOS JOGADORES ==')
 for i, v in enumerate(ranking):
     print(f'{i + 1}º lugar: {v[0]} com {v[1]}.')
     sleep(0.5)
