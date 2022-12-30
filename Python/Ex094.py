ed48b3e729b839212c4eb01c3b876d13ea17fce8
Felipe Araujo <felipearaujodeassis@gmail.com>
2022-12-30 14:22:45 -0300

    update ex 094 de python

--- Python/Ex094.py
+++ Python/Ex094.py
@@ -1,6 +1,38 @@
-"""Exercício 094 - Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados
-de cada pessoa em um dicionário e todos os dicionários em uma lista. No final, mostre:
-a) Quantas pessoas foram cadastradas
-b) A média de idade do grupo.
-c) Uma lista com todas as mulheres.
-d) Uma lista com todas as pessoas com idade acima da média."""
+galera = list()
+pessoa = dict()
+soma = média = 0
+while True:
+    pessoa.clear()
+    pessoa['nome'] = input('Nome: ').strip()
+    while True:
+        pessoa['sexo'] = input('Sexo [M/F]: ').strip().upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Por favor, digite apenas M ou F.')
+    pessoa['idade'] = int(input('Idade: ').strip())
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())
+    while True:
+        resposta = input('Quer continuar [S/N]? ').strip().upper()[0]
+        if resposta in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resposta == 'N':
+        break
+print('-=' * 30)
+print(f'A) Ao todo temos {len(galera)} pessoas cadastradas.')
+média = soma / len(galera)
+print(f'B) A média de idade é de {média:5.2f} anos')
+print(f'C) As mulheres cadastradas foram ', end='')
+for p in galera:
+    if p['sexo'] == 'F':
+        print(f'{p["nome"]} ', end='')
+print()
+print(f'D) Lista das pessoas que estão acima da média:')
+for p in galera:
+    if p['idade'] >= média:
+        print('    ', end='')
+        for k, v in p.items():
+            print(f'{k} = {v}', end='')
+            print()
+print('<< ENCERRADO >>')
